bbfc892dbe1d7c2f7ce36a0a540168ddb6ff3e60
AidanFeather <36227521+AidanFeather@users.noreply.github.com>
2020-04-02 16:51:27 +0100

    Sounds added
    Have added the thruster sounds, weapon sounds, main menu music and a health warning that plays when the player gets below 20 health

--- ProjectExodus/Intermediate/Build/Win64/ProjectExodusEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ ProjectExodus/Intermediate/Build/Win64/ProjectExodusEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/Jason/Documents/GitHub/Project-Exodus/ProjectExodus/Intermediate/Build/Win64/ProjectExodusEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "F:/01 - User Profiles/dudey/Documents/GitHub/Project-Exodus/ProjectExodus/Intermediate/Build/Win64/ProjectExodusEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
