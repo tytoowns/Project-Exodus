befd117b4c99d0a56f01d1b9dc22a042c20d9183
HexDnB <47872206+HexDnB@users.noreply.github.com>
2020-05-07 22:53:40 +0100

    Failsafe, enemy tweak
    A failsafe has been added in the event the player leaves the combat arena
    
    Tweaks to enemies due to rare pathfinding bug

--- ProjectExodus/Source/ProjectExodus/FlockManager.h
+++ ProjectExodus/Source/ProjectExodus/FlockManager.h
@@ -28,7 +28,7 @@ protected:
 	float boidSpeed = 150000.0f;
 	int flockSize = 15;
 	float interpSpeed = 0.02f;
-	float rotationTolerance = 0.01f;
+	float rotationTolerance = 0.02f;
 
 	TArray<AActor*> boidList;
 	TArray<FVector> hexagonLocations;
